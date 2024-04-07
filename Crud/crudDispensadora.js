7011126549d49ef52042f3b6d7a615eed0b6a1b3
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-04-06 19:07:38 -0600

    add: first commit

--- Crud/crudDispensadora.js
+++ Crud/crudDispensadora.js
@@ -3,14 +3,31 @@ const express = require('express');
 
 const faqsDispensadora = express.Router();
 
-// Obtener la información de "FAQs"
-faqsDispensadora.get('/no-asignados', async (req, res, next) => {
+// Endpoint para actualizar un documento específico
+faqsDispensadora.put('/:id', async (req, res, next) => {
     const collection = req.db.collection("dispensadoras");
+    const dispensadorId = req.params.id;
+    const updatedDispensador = req.body;
+
+    // Validación básica de los datos recibidos
+    if (!ObjectId.isValid(dispensadorId)) {
+        return res.status(400).json({ message: "ID de dispensador no válido" });
+    }
+
     try {
-        const dispositivosNoAsignados = await collection.find({ assignedTo: null }).toArray();
-        res.json(dispositivosNoAsignados);
+        const result = await collection.updateOne(
+            { _id: new ObjectId(dispensadorId) },
+            { $set: updatedDispensador }
+        );
+
+        if (result.matchedCount === 1) {
+            return res.json({ message: "Documento actualizado correctamente" });
+        } else {
+            return res.status(404).json({ message: "Documento no encontrado" });
+        }
     } catch (error) {
-        next(error);
+        console.error('Error al actualizar el documento:', error);
+        return res.status(500).json({ message: "Error interno del servidor" });
     }
 });
 
